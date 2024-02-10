4c34d47fe348cca707e526b3dd8bcbf00a4e16ac
root <root@localhost.localdomain>
2024-02-10 06:57:35 +0000

    new6

---
+++ assign2/codes/graphg.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+# Load data from the file
+data = []
+with open("generaltermdata.txt", "r") as file:
+    for line in file:
+        n, a = map(int, line.split())
+        data.append((n, a))
+
+# Extract x and y values from data
+x_values = [point[0] for point in data]
+y_values = [point[1] for point in data]
+
+# Plotting the points
+plt.scatter(x_values, y_values, color='blue')
+plt.title('Sequence')
+plt.xlabel('Index (n)')
+plt.ylabel('Term (a)')
+plt.grid(True)
+plt.show()
+plt.savefig('generalterm.png')
---
+++ assign2/codes/sumplot.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+def calculate_a(n_values, d):
+    return [((n + 1) / 2) * (4 + 7 * n) for n in n_values]
+
+def plot_function(n_values, a_values):
+    plt.plot(n_values, a_values, marker='o', linestyle='-')
+    plt.xlabel('n')
+    plt.ylabel('a')
+    plt.title('Sum of n terms')
+    plt.grid(True)
+    plt.show()
+    plt.savefig('sumplot.png')
+
+# Define the range of n values (only integral values)
+n_values = range(1, 40, 1)  
+
+# Define the common difference (d)
+d = 1  # Example: d = 1
+
+# Calculate the corresponding 'a' values
+a_values = calculate_a(n_values, d)
+
+# Plot the function
+plot_function(n_values, a_values)
