69a72ceb56bd517ebe912482bb2dbd7482d8e2bd
root <root@localhost.localdomain>
2024-02-19 04:13:55 +0000

    new8

--- assign2/python/sumplot.py
+++ assign2/python/sumplot.py
@@ -4,13 +4,13 @@ def calculate_a(n_values, d):
     return [((n + 1) / 2) * (4 + 7 * n) for n in n_values]
 
 def plot_function(n_values, a_values):
-    plt.plot(n_values, a_values, marker='o', linestyle='-')
+    plt.stem(n_values, a_values, linefmt='-', markerfmt='o', basefmt=' ')
     plt.xlabel('n')
-    plt.ylabel('a')
-    plt.title('Plot of a=((n+1)/2)*(4+7*n)')
+    plt.ylabel('y(n)')
+    plt.title('Stem plot of y(n)')
     plt.grid(True)
     plt.show()
-    plt.sav
+    plt.savefig('sumplot.png')
 
 # Define the range of n values (only integral values)
 n_values = range(1, 40, 1)  
@@ -23,3 +23,4 @@ a_values = calculate_a(n_values, d)
 
 # Plot the function
 plot_function(n_values, a_values)
+
