07fe2ac5dc3c5946b4982db71c514b9458aaba26
root <root@localhost.localdomain>
2024-03-08 18:19:48 +0000

    @new2@

--- gate2/codes/plot.py
+++ gate2/codes/plot.py
@@ -1,31 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import signal
 
-# Define the transfer function
-num = [14.4]
-den = [0.1, 1, 14.4]
-sys = signal.TransferFunction(num, den)
+# Load data from magnitude_data.txt and phase_data.txt
+mag_data = np.loadtxt('magnitude_data.txt', delimiter=',', skiprows=1)
+phase_data = np.loadtxt('phase_data.txt', delimiter=',', skiprows=1)
 
-# Create a frequency range
-w = np.logspace(-1, 2, 1000)
+# Create a figure with two subplots
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 
-# Calculate the magnitude and phase of the transfer function
-w, mag, phase = signal.bode(sys, w)
-plt.figure()
+# Plot magnitude in the first subplot
+ax1.semilogx(mag_data[:, 0], mag_data[:, 1])
+ax1.set_ylabel(r'$20\log{|T|}$')
+ax1.grid(True)
 
-# Magnitude subplot
-plt.subplot(2, 1, 1)
-plt.semilogx(w, mag)
-plt.ylabel(r'$20\log{|T|}$')
-plt.grid(True)
+# Plot phase in the second subplot
+ax2.semilogx(phase_data[:, 0], phase_data[:, 1])
+ax2.set_xlabel(r'$\omega$')
+ax2.set_ylabel('Phase [degrees]')
+ax2.grid(True)
 
-# Phase subplot
-plt.subplot(2, 1, 2)
-plt.semilogx(w, phase)
-plt.xlabel(r' $\omega$')
-plt.ylabel('Phase [degrees]')
-plt.grid(True)
-
-plt.show()
+# Adjust layout and save the plot
+plt.tight_layout()
 plt.savefig("plot.png")
+plt.show()
